de6395db3478aeec769a4929fd11dc1ad4e0a30b
Noble Obioma <nobioma1@gmail.com>
2019-06-19 13:14:58 +0100

    Add actionCreator to fetchCharacters, invoke actionCreator in mount, connect store, add loading text

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 
-import { CharacterList } from "../components";
-// import actions
+import { CharacterList } from '../components';
+import { fetchCharacters } from '../actions';
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
-
   componentDidMount() {
-    // call our action
+    this.props.fetchCharacters();
   }
 
   render() {
-    if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
+    if (this.props.isFetching) {
+      return (
+        <div>
+          <h2>Fetching Characters...</h2>
+        </div>
+      );
     }
     return (
       <div className="CharactersList_wrapper">
@@ -25,11 +25,14 @@ class CharacterListView extends React.Component {
   }
 }
 
-// our mapStateToProps needs to have two properties inherited from state
-// the characters and the fetching boolean
+const mapStateToProps = ({ charsReducer }) => ({
+  characters: charsReducer.characters,
+  isFetching: charsReducer.isFetching,
+});
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps,
   {
-    /* action creators go here */
-  }
+    fetchCharacters,
+  },
 )(CharacterListView);
